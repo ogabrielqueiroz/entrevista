489302161232b25f75fdddc9f9b57ff2217de773
ogabrielqueiroz <91276497+ogabrielqueiroz@users.noreply.github.com>
2023-04-04 19:46:20 -0300

    Update stringtarget.R

--- stringtarget.R
+++ stringtarget.R
@@ -1,6 +1,16 @@
-string <- "String para a Target"
-inverted_string <- paste(rev(strsplit(string, "")[[1]]), collapse="")
-print(inverted_string)
-string <- "String para a Target"
-inverted_string <- paste(rev(strsplit(string, "")[[1]]), collapse="")
-print(inverted_string)
+string_original <- "String para a target"
+
+
+inverter_string <- function(string) {
+  tamanho_string <- nchar(string)  
+  string_invertida <- ""
+  
+  for (i in tamanho_string:1) { 
+    string_invertida <- paste0(string_invertida, substr(string, i, i))
+  }
+  
+  return(string_invertida)
+}
+
+string_invertida <- inverter_string(string_original)
+cat(string_invertida)
