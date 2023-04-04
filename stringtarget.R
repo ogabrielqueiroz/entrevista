fe890924ba643e8da18fdbca3ae19e16dc9e38ba
ogabrielqueiroz <91276497+ogabrielqueiroz@users.noreply.github.com>
2023-04-04 19:39:45 -0300

    Add files via upload

---
+++ stringtarget.R
@@ -0,0 +1,6 @@
+string <- "String para a Target"
+inverted_string <- paste(rev(strsplit(string, "")[[1]]), collapse="")
+print(inverted_string)
+string <- "String para a Target"
+inverted_string <- paste(rev(strsplit(string, "")[[1]]), collapse="")
+print(inverted_string)
